3f5243efb6085054427240cc1ae3264a659527de
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2023-10-13 19:11:54 +0200

    reconcile metadata from old roue into new one

---
+++ reconcile-new-old-roues/reconcile.py
@@ -0,0 +1,39 @@
+from sys import stderr
+from csv import DictReader
+from casanova import enricher
+from ural.lru import LRUTrie
+
+old_medias = {}
+old_headers = []
+trie = LRUTrie(strip_trailing_slash=True)
+with open("medias_Fr_2020.csv") as f:
+    for row in DictReader(f):
+        if not old_headers:
+            old_headers = ["old_%s" % k for k in row.keys()]
+        for url in row["prefixes"].split("|"):
+            trie.set(url, row["webentity_id"])
+        old_medias[row["webentity_id"]] = row
+
+
+with open("medias_Fr_2023.csv") as f, \
+     open("medias_Fr_2023+metas2020.csv", "w") as of:
+    rows = enricher(f, of, add=old_headers)
+    name_pos = rows.headers["NAME"]
+    prefixes_pos = rows.headers["PREFIXES AS URL"]
+    for row in rows:
+        matches = []
+        for url in row[prefixes_pos].split(" "):
+            old = trie.match(url)
+            if old and old not in matches:
+                matches.append(old)
+
+        olddata = [None for k in old_headers]
+        if len(matches) == 1:
+            olddata = [old_medias[matches[0]][k.replace("old_", "")] for k in old_headers]
+        elif len(matches) > 1:
+            print("WARNING: found multiple old WebEntities matching %s:" % row[name_pos], row[prefixes_pos], [old_medias[i]["name"] for i in matches], file=stderr)
+
+        else:
+            print("INFO: could not find %s in old WebEntities:" % row[name_pos], row[prefixes_pos], file=stderr)
+        rows.writerow(row, olddata)
+
